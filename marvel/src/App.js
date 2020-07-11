68abcf7ebced852a3f34d5c0483f669a508d446d
agomez99 <austine.gomez@yahoo.com>
2020-07-10 22:04:20 -0500

    loads characters

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,254 +1,163 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-var md5 = require('md5');
-
-var publickey = '228a2cac6d893dce20244bdab584d41a';
-var privatekey = '2a1031bac357948b9d36de0d59e6f2db649cc3f1';
-var ts = new Date().getTime();
-var stringToHash = ts + privatekey + publickey;
-var hash = md5(stringToHash);
-var baseUrl = 'https://gateway.marvel.com:443/v1/public/characters';
-var limit = 100;
-var url = baseUrl + '?limit=' + limit + '&ts=' + ts + '&apikey=' + publickey + '&hash=' + hash;
-
-
-fetch(url)
-.then((resp) => resp.json())
-.then(function(mydata){
-  console.log(mydata.data.results)
-
-})
-
-.catch(function(data){
-
-});
-
-
-
-
-
-
-
-const data = {
-  "code": 200,
-  "status": "Ok",
-  "copyright": "© 2020 MARVEL",
-  "attributionText": "Data provided by Marvel. © 2020 MARVEL",
-  "attributionHTML": "<a href=\"http://marvel.com\">Data provided by Marvel. © 2020 MARVEL</a>",
-  "etag": "791d563e62252f05660d24c630153799e1d3ac81",
-  "data": {
-    "offset": 0,
-    "limit": 20,
-    "total": 1,
-    "count": 1,
-    "results": [
-      {
-        "id": 14297,
-        "digitalId": 7768,
-        "title": "X-Men (1991) #113",
-        "issueNumber": 113,
-        "variantDescription": "",
-        "description": null,
-        "modified": "2020-02-28T12:57:42-0500",
-        "isbn": "",
-        "upc": "759606017721011311",
-        "diamondCode": "FEB011586",
-        "ean": "",
-        "issn": "",
-        "format": "Comic",
-        "pageCount": 0,
-        "textObjects": [],
-        "resourceURI": "http://gateway.marvel.com/v1/public/comics/14297",
-        "urls": [
-          {
-            "type": "detail",
-            "url": "http://marvel.com/comics/issue/14297/x-men_1991_113?utm_campaign=apiRef&utm_source=228a2cac6d893dce20244bdab584d41a"
-          },
-          {
-            "type": "reader",
-            "url": "http://marvel.com/digitalcomics/view.htm?iid=7768&utm_campaign=apiRef&utm_source=228a2cac6d893dce20244bdab584d41a"
-          }
-        ],
-        "series": {
-          "resourceURI": "http://gateway.marvel.com/v1/public/series/2265",
-          "name": "X-Men (1991 - 2001)"
-        },
-        "variants": [],
-        "collections": [
-          {
-            "resourceURI": "http://gateway.marvel.com/v1/public/comics/1951",
-            "name": "X-Men: Eve of Destruction (Trade Paperback)"
-          },
-          {
-            "resourceURI": "http://gateway.marvel.com/v1/public/comics/60",
-            "name": "X-Statix Vol. 4: X-Statix Vs. the Avengers (Trade Paperback)"
-          }
-        ],
-        "collectedIssues": [],
-        "dates": [
-          {
-            "type": "onsaleDate",
-            "date": "2001-06-01T00:00:00-0400"
-          },
-          {
-            "type": "focDate",
-            "date": "-0001-11-30T00:00:00-0500"
-          },
-          {
-            "type": "unlimitedDate",
-            "date": "2010-05-26T00:00:00-0400"
-          }
-        ],
-        "prices": [
-          {
-            "type": "printPrice",
-            "price": 0
-          }
-        ],
-        "thumbnail": {
-          "path": "http://i.annihil.us/u/prod/marvel/i/mg/c/20/4bb7b64d1d3b8",
-          "extension": "jpg"
-        },
-        "images": [
-          {
-            "path": "http://i.annihil.us/u/prod/marvel/i/mg/c/20/4bb7b64d1d3b8",
-            "extension": "jpg"
-          }
-        ],
-        "creators": {
-          "available": 7,
-          "collectionURI": "http://gateway.marvel.com/v1/public/comics/14297/creators",
-          "items": [
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/8542",
-              "name": "Gerry Alanguilan",
-              "role": "inker"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/430",
-              "name": "Edgar Delgado",
-              "role": "inker"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/471",
-              "name": "Dexter Vines",
-              "role": "inker"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/13348",
-              "name": "Liquid! Color",
-              "role": "colorist"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/3637",
-              "name": "Scott Lobdell",
-              "role": "writer"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/350",
-              "name": "Richard Starkings",
-              "role": "letterer"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/creators/758",
-              "name": "Leinil Francis Yu",
-              "role": "penciler"
-            }
-          ],
-          "returned": 7
-        },
-        "characters": {
-          "available": 6,
-          "collectionURI": "http://gateway.marvel.com/v1/public/comics/14297/characters",
-          "items": [
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009257",
-              "name": "Cyclops"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009267",
-              "name": "Dazzler"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009417",
-              "name": "Magneto"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009504",
-              "name": "Professor X"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009718",
-              "name": "Wolverine"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/characters/1009726",
-              "name": "X-Men"
-            }
-          ],
-          "returned": 6
-        },
-        "stories": {
-          "available": 3,
-          "collectionURI": "http://gateway.marvel.com/v1/public/comics/14297/stories",
-          "items": [
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/stories/29146",
-              "name": "X-MEN (1991) #113",
-              "type": "cover"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/stories/29147",
-              "name": "Eve of Destruction Conclusion",
-              "type": "interiorStory"
-            },
-            {
-              "resourceURI": "http://gateway.marvel.com/v1/public/stories/162486",
-              "name": "story from X-Men (1991) #113",
-              "type": "interiorStory"
-            }
-          ],
-          "returned": 3
-        },
-        "events": {
-          "available": 0,
-          "collectionURI": "http://gateway.marvel.com/v1/public/comics/14297/events",
-          "items": [],
-          "returned": 0
-        }
-      }
-    ]
-  }
-}
-
-const {thumbnail } = data.data.results[0]
-const {creators} = data.data.results[0]
-
-const creator = creators.items.map(function (items, index) {
-return <p key ={index}>{items.name}</p>
-})
-
-
-
-
-
+import { ResultsList } from './components/ResultsList';
+import { ResultDetails } from './components/ResultDetails';
+import { SearchBar } from './components/SearchBar';
+import { Error } from './components/Error';
+import { Loading } from './components/Loading';
+import { LoadMore } from './components/LoadMore';
+import { MarvelService } from './services/MarvelService';
 
 class App extends Component {
-  render() {
+  // --------------------------------------------------
+  // SETUP
+  // --------------------------------------------------
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchTerm: '',
+      results: [],
+      canLoadMore: false,
+      selectedResult: null,
+    };
+
+    this.fetchCharacters = this.fetchCharacters.bind(this);
+    this.fetchCharacter = this.fetchCharacter.bind(this);
+    this.fetchMoreCharacters = this.fetchMoreCharacters.bind(this);
+
+    this.marvelService = new MarvelService({
+      apiKey: this.props.apiKey,
+    });
+  }
+
+  // --------------------------------------------------
+  // RENDER
+  // --------------------------------------------------
+  render() {
+    const resultsElem = this.state.hasError
+      ? <Error />
+      : this.state.isLoading
+        ? <Loading searchTerm={ this.state.searchTerm } />
+        : (
+          <ResultsList
+            results={ this.state.results }
+            searchTerm={ this.state.searchTerm }
+            onResultClick={ this.fetchCharacter }
+          />
+        );
+
+    const loadMoreElem = this.state.canLoadMore
+      ? <LoadMore onClick={ this.fetchMoreCharacters }/>
+      : '';
+
+    const detailsElem = this.state.selectedResult
+      ? (
+        <ResultDetails
+          image={ this.state.selectedResult.thumbnail.path +  '.' + this.state.selectedResult.thumbnail.extension }
+          title={ this.state.selectedResult.name }
+          description={ this.state.selectedResult.description }
+          stories={ this.state.selectedResult.stories }
+          urls={ this.state.selectedResult.urls }
+          onClose={ () => this.setState({ selectedResult: null } )}
+        />
+      )
+      : '';
 
     return (
-      <div>
-        <h1 className="header">Marvel</h1>
-        <div>
-          <p>{data.data.results[0].title}</p>
-          <img className='thumb' src={`${thumbnail.path}.${thumbnail.extension}`} alt="" ></img>
-          <h1>Creators</h1>
-          <span>{creator}</span>
-        </div>
-      </div>
-
+      <section className="app">
+        <SearchBar
+          searchTerm={ this.state.searchTerm }
+          onSubmit={ (searchTerm) => this.setState({ searchTerm }) }
+        />
+        { resultsElem }
+        { loadMoreElem }
+        { detailsElem }
+      </section>
     );
   }
+
+  // --------------------------------------------------
+  // LIFECYCLE
+  // --------------------------------------------------
+  componentDidUpdate(_, prevState) {
+    const searchTerm = this.state.searchTerm;
+    const prevSearchTerm = prevState.searchTerm;
+
+    if (
+      searchTerm
+      && (searchTerm !== prevSearchTerm)
+    ) {
+      this.fetchCharacters();
+    }
+  }
+
+  // --------------------------------------------------
+  // FETCHING CHARACTERS
+  // --------------------------------------------------
+  fetchCharacters() {
+    //console.warn('Whoops, it looks like this method hasn\'t been implemented yet');
+
+    // TODO:
+    // Put the application into a loading state.
+    this.setState({ isLoading: true });
+
+    // Invoke the `getCharacters()` method on the marvel service.
+    // Pass in the current `searchTerm` as `nameStartsWith`,
+    this.marvelService.getCharacters({
+      nameStartsWith: this.state.searchTerm,
+    })
+      .then((data) => {
+        // Update the application state using the resulting data.
+        // Remove the loading state.
+        this.setState({
+          results: data.results,
+          canLoadMore: data.total > data.offset + data.count,
+          isLoading: false,
+        });
+      })
+      .catch((err) => {
+        // Handle potential errors.
+        console.error(err);
+        this.setState({ hasError: true });
+      });
+  }
+
+  fetchCharacter(id) {
+    // console.warn('Whoops, it looks like this method hasn\'t been implemented yet');
+
+    // TODO:
+    // Invoke the `getCharacter()` method on the marvel service.
+    // Pass in the `id`.
+    this.marvelService.getCharacter(id)
+      .then((data) => {
+        // Update the application state using the resulting data.
+        this.setState({ selectedResult: data.results[0] });
+      })
+      .catch((err) => {
+        // Handle potential errors.
+        console.error(err);
+        this.setState({ hasError: true });
+      });
+  }
+
+  fetchMoreCharacters() {
+    this.marvelService.getCharacters({
+      nameStartsWith: this.state.searchTerm,
+      offset: this.state.results.length,
+    })
+      .then((data) => {
+        this.setState({
+          results: [...this.state.results, ...data.results],
+          canLoadMore: data.total > data.offset + data.count,
+        });
+      })
+      .catch((err) => {
+        // Handle potential errors.
+        console.error(err);
+        this.setState({ hasError: true });
+      });
+  }
 }
+
 export default App;
---
+++ marvel/src/services/MarvelService.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+
+export class MarvelService {
+
+  // --------------------------------------------------
+  // ENDPOINTS
+  // --------------------------------------------------
+  static get ENDPOINTS() {
+    return {
+      comic: 'https://gateway.marvel.com:443/v1/public/comics',
+      comics: 'https://gateway.marvel.com:443/v1/public/comics',
+      character: 'https://gateway.marvel.com:443/v1/public/characters',
+      characters: 'https://gateway.marvel.com:443/v1/public/characters',
+    };
+  }
+
+  // --------------------------------------------------
+  // SETUP
+  // --------------------------------------------------
+  constructor(config) {
+    this.apiKey = config.apiKey;
+  }
+
+  // --------------------------------------------------
+  // AUTHENTICATION
+  // --------------------------------------------------
+  getAuthConfig() {
+    return { apikey: this.apiKey };
+  }
+
+  // --------------------------------------------------
+  // CHARACTERS-RELATED METHODS
+  // --------------------------------------------------
+  getCharacters(config = {}) {
+    console.warn('Whoops, it looks like this method hasn\'t been implemented yet.');
+    // TODO:
+    // - Create the `params` object.
+    // - Extract the correct endpoint from `ENDPOINTS`.
+    // - Dispatch a request using `axios.get()`.
+    // - Parse and return the response.
+    const params = { ...config, ...this.getAuthConfig() };
+
+    const endpoint = MarvelService.ENDPOINTS.characters;
+    
+    return axios.get(endpoint, { params: params })
+    .then((response) => {
+      return response.data.data;
+    });
+
+  }
+
+  getCharacter(id, config = {}) {
+    // console.warn('Whoops, it looks like this method hasn\'t been implemented yet.');
+
+    // TODO:
+    // - Create the `params` object.
+    const params = { ...config, ...this.getAuthConfig() };
+
+    // - Extract the correct endpoint from `ENDPOINTS`; add the `id`.
+    const endpoint = MarvelService.ENDPOINTS.character + '/' + id;
+
+    // - Dispatch a request using `axios.get()`.
+    return axios.get(endpoint, { params: params })
+      .then((response) => {
+        // - Parse and return the response.
+        return response.data.data;
+      });
+  }
+}
