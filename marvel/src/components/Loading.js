48a9141589d833b26381b7de3d5b198e08bf9e7b
Austine Gomez <austine.gomez@yahoo.com>
2020-08-22 18:05:50 -0500

    added boostrap cdn

--- marvel/src/components/Loading.js
+++ marvel/src/components/Loading.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import loading from './spin.gif'
+import './loading.css'
 export class Loading extends Component {
   render() {
     return (
       <div className="loading">
-        <h1>{ 'Loading results for "' + this.props.searchTerm + '".' }</h1>
-      </div>
+        {/* <h1>{ 'Loading results for "' + this.props.searchTerm + '".' }</h1> */}
+        <img src={loading} alt="loading" className="loading-img"/>
+        </div>
     );
   }
 }
--- marvel/src/components/ResultDetails.js
+++ marvel/src/components/ResultDetails.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-
+import './results.css'
 export class ResultDetails extends Component {
   render() {
     return (
       <article className="result-details">
         <div className="result-details__backdrop"></div>
         <div className="result-details__info">
-          <div className="result-details__info__header">
-            <figure>
-              <img src={ this.props.image } alt={ this.props.title } />
-            </figure>
-          </div>
+         
           <div className="result-details__info__body">
-            <div className="result-details__info__body__content">
-              <h1>{ this.props.title }</h1>
-              <h1>{ this.props.name }</h1>
+            <div className="result-details__info__body__content" style={{height:"9600px"}}>
 
-              <p><strong>Appears In: { this.props.stories.available || 0 } stories</strong></p>
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm">
+                  <img src={ this.props.image } alt={ this.props.title } className="imgpro"/>
+                {/* <h1>{ this.props.title }</h1>
+                  <h1>{ this.props.name }</h1> */}                  
+                  </div>
+                  <div className="col-sm">
+                  <p><strong>Appears In: { this.props.stories.available || 0 } stories</strong></p>
               <p>{ this.props.description }</p>
               <p><strong>Links:</strong></p>
               <ul>
                 { this.props.urls.map(({ url, type }, i) => <li key={ i }><a href={ url } target="_blank" rel="noopener noreferrer">{ type }</a></li>) }
-              </ul>
+              </ul>                 
+              </div>
+                </div>
+              </div>
             </div>
             <button onClick={ this.props.onClose }></button>
           </div>
